da1477102384d8ff762ae02e06bcdbe4f5be73ed
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2023-09-24 18:22:46 -0700

    Updated menu design

--- src/components/WindowMenu.tsx
+++ src/components/WindowMenu.tsx
@@ -4,6 +4,15 @@ import { open } from "@tauri-apps/plugin-shell";
 import { getCurrent } from "@tauri-apps/plugin-window";
 import * as Menubar from "@radix-ui/react-menubar";
 import { error, info } from "@tauri-apps/plugin-log";
+import {
+  ActivityLogIcon,
+  ClipboardCopyIcon,
+  DesktopIcon,
+  EnterFullScreenIcon,
+  ExitFullScreenIcon,
+  ExitIcon,
+} from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
 
 export interface WindowMenuProps {
   showWelcomeScreen: () => void;
@@ -11,6 +20,15 @@ export interface WindowMenuProps {
 
 const WindowMenu = ({ showWelcomeScreen }: WindowMenuProps) => {
   const currentWindow = getCurrent();
+  const [isFullscreen, setFullscreen] = useState(false);
+
+  useEffect(() => {
+    const getIsFullscreen = async () => {
+      setFullscreen(await currentWindow.isFullscreen());
+    };
+
+    getIsFullscreen();
+  }, [currentWindow]);
 
   const handleQuitApp = async () => {
     try {
@@ -23,8 +41,8 @@ const WindowMenu = ({ showWelcomeScreen }: WindowMenuProps) => {
 
   const handleToggleFullscreen = async () => {
     try {
-      const isFullscreen = await currentWindow.isFullscreen();
       await currentWindow.setFullscreen(!isFullscreen);
+      setFullscreen(!isFullscreen);
     } catch (err) {
       error(`Failed to toggle fullscreen: ${err}`);
     }
@@ -84,16 +102,17 @@ const WindowMenu = ({ showWelcomeScreen }: WindowMenuProps) => {
         </Menubar.Trigger>
         <Menubar.Portal>
           <Menubar.Content
-            className="flex flex-col gap-1 px-4 py-2 bg-white border border-gray-100 rounded-lg shadow-lg"
+            className="flex flex-col gap-2 px-4 py-3 bg-white border border-gray-100 rounded-lg shadow-lg"
             align="start"
             sideOffset={5}
             alignOffset={-3}
           >
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleQuitApp}
             >
-              Quit Application
+              <ExitIcon className="my-auto text-gray-600" />
+              <p className="text-sm text-gray-600">Quit Application</p>
             </Menubar.Item>
           </Menubar.Content>
         </Menubar.Portal>
@@ -105,16 +124,24 @@ const WindowMenu = ({ showWelcomeScreen }: WindowMenuProps) => {
         </Menubar.Trigger>
         <Menubar.Portal>
           <Menubar.Content
-            className="flex flex-col gap-1 px-4 py-2 bg-white border border-gray-100 rounded-lg shadow-lg"
+            className="flex flex-col gap-2 px-4 py-3 bg-white border border-gray-100 rounded-lg shadow-lg"
             align="start"
             sideOffset={5}
             alignOffset={-3}
           >
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleToggleFullscreen}
             >
-              Toggle Fullscreen
+              {isFullscreen ? (
+                <ExitFullScreenIcon className="my-auto text-gray-600" />
+              ) : (
+                <EnterFullScreenIcon className="my-auto text-gray-600" />
+              )}
+
+              <p className="text-sm text-gray-600">
+                {isFullscreen ? "Exit fullscreen" : "Enable fullscreen"}
+              </p>
             </Menubar.Item>
           </Menubar.Content>
         </Menubar.Portal>
@@ -126,40 +153,53 @@ const WindowMenu = ({ showWelcomeScreen }: WindowMenuProps) => {
         </Menubar.Trigger>
         <Menubar.Portal>
           <Menubar.Content
-            className="flex flex-col gap-1 px-4 py-2 bg-white border border-gray-100 rounded-lg shadow-lg"
+            className="flex flex-col gap-2 px-4 py-3 bg-white border border-gray-100 rounded-lg shadow-lg"
             align="start"
             sideOffset={5}
             alignOffset={-3}
           >
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleShowWelcomeScreen}
             >
-              Show welcome screen
+              <ExitIcon className="my-auto text-gray-600" />
+              <p className="text-sm text-gray-600">Show welcome screen</p>
             </Menubar.Item>
+
+            <Menubar.Separator className="h-px bg-gray-300 my-1" />
+
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleCopyLogDir}
             >
-              Copy log directory to clipboard
+              <ClipboardCopyIcon className="my-auto text-gray-600" />
+              <p className="text-sm text-gray-600">
+                Copy log directory to clipboard
+              </p>
             </Menubar.Item>
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleOpenWebviewLogFile}
             >
-              Open webview log file
+              <DesktopIcon className="my-auto text-gray-600" />
+              <p className="text-sm text-gray-600">Open webview log file</p>
             </Menubar.Item>
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleOpenBackendLogFile}
             >
-              Open backend log file
+              <ActivityLogIcon className="my-auto text-gray-600" />
+              <p className="text-sm text-gray-600">Open backend log file</p>
             </Menubar.Item>
+
+            <Menubar.Separator className="h-px bg-gray-300 my-1" />
+
             <Menubar.Item
-              className="text-sm text-gray-600 cursor-pointer"
+              className="flex flex-row gap-3 cursor-pointer"
               onSelect={handleReportBug}
             >
-              Report a bug
+              <ExitIcon className="my-auto text-gray-600" />
+              <p className="text-sm text-gray-600">Report a bug</p>
             </Menubar.Item>
           </Menubar.Content>
         </Menubar.Portal>
